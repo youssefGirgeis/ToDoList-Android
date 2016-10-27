e227d2561b665612c9943c692f7b158da265f1e8
Youssef Girgeis <youssefgirgeis@gmail.com>
2016-10-27 00:06:26 -0400

    add a list view

--- app/src/main/java/com/youssefgirgeis/todolist/MainActivity.java
+++ app/src/main/java/com/youssefgirgeis/todolist/MainActivity.java
@@ -2,6 +2,10 @@ package com.youssefgirgeis.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ListView todoList = (ListView) findViewById(R.id.todo_list);
+
+        String[] values = new String[] {"millk", "asda", "aaaa", "wwww"};
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        for(int i=0; i<values.length; i++){
+
+            list.add(values[i]);
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                list
+        );
+
+        todoList.setAdapter(adapter);
     }
 }
